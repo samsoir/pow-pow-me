0431f2fabec3b1c36891ac734de78d289613e93d
Sam de Freyssinet <sam@def.reyssi.net>
2014-04-27 12:43:10 -0500

    Initial Commit

---
+++ Pow Pow Me/MDFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MDFAppDelegate.h
+//  Pow Pow Me
+//
+//  Created by Sam de Freyssinet on 27/04/2014.
+//  Copyright (c) 2014 Maison de Freyssinet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pow Pow Me/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pow Pow Me
+//
+//  Created by Sam de Freyssinet on 27/04/2014.
+//  Copyright (c) 2014 Maison de Freyssinet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MDFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MDFAppDelegate class]));
+    }
+}
